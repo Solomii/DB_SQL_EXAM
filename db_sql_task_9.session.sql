0c8bda87e536938b844f4d4ab1a91181ecd6d898
Solomiia Kushniryk <kushniryksolomiya@gmail.com>
2023-11-02 17:02:47 +0200

    Add cashback to users, added 10% to the cashback for a user with the role of customer (task 10)

--- db_sql_task_9.session.sql
+++ db_sql_task_9.session.sql
@@ -1,5 +1,5 @@
 DROP TABLE IF EXISTS "roles" cascade;
-DROP TABLE IF EXISTS "users_with_roles";
+DROP TABLE IF EXISTS "users_with_roles" cascade;
 
 CREATE TABLE "roles"(
   "role_id" SERIAL PRIMARY KEY,
@@ -16,41 +16,45 @@ CREATE TABLE "users_with_roles" (
     "firstName" VARCHAR(64) NOT NULL CHECK("firstName" != ''),
     "lastName" VARCHAR(64) NOT NULL CHECK("lastName" != ''),
     "email" VARCHAR(256) NOT NULL CHECK("email" != '') UNIQUE,
-    "role" INT REFERENCES "roles"("role_id")
+    "role" INT REFERENCES "roles"("role_id"),
+    "cashback" NUMERIC(8,2) CHECK("cashback" >=0 AND "cashback"<1000000),
+    "currency" VARCHAR(5) NOT NULL
   );
   
-INSERT INTO "users_with_roles"("firstName", "lastName", "email", "role")
-VALUES ('Tom', 'Test', 'tom@gamil.com', 1),
-  ('Mary', 'Smith', 'test1@gamil.com', 1),
-  ('Ava', 'Jonson', 'test2@gamil.com', 2),
-  ('Emma', 'Li', 'test3@gamil.com', 2),
-  ('Wiliam', 'Jones', 'test4@gamil.com', 2),
-  ('Thomas', 'Roy', 'test5@gamil.com', 2),
+INSERT INTO "users_with_roles"("firstName", "lastName", "email", "role", "cashback", "currency" )
+VALUES ('Tom', 'Test', 'tom@gamil.com', 1, 0, 'uah'),
+  ('Mary', 'Smith', 'test1@gamil.com', 1, 0, 'uah'),
+  ('Ava', 'Jonson', 'test2@gamil.com', 2, 0, 'uah'),
+  ('Emma', 'Li', 'test3@gamil.com', 2, 0, 'uah'),
+  ('Wiliam', 'Jones', 'test4@gamil.com', 2, 0,'uah'),
+  ('Thomas', 'Roy', 'test5@gamil.com', 2, 0,'uah'),
   (
     'Emily',
     'White',
     'test6@gamil.com',
-    3
+    3, 0, 'uah'
   ),
   (
     'James',
     'Wilson',
     'test7@gamil.com',
-    3
+    3, 0,'uah'
   ),
-  ('Mary', 'Test', 'test8@gamil.com', 3),
-  ('David', 'Jones', 'test9@gamil.com', 2),
-  ('Joe', 'Test', 'test10@gamil.com', 1),
-  ('Olivia', 'Test', 'test11@gamil.com', 2),
-  ('Mary', 'Test', 'test12@gamil.com', 2),
-  ('Tom', 'Test', 'test13@gamil.com', 1),
-  ('Lily', 'Test', 'test14@gamil.com', 1),
+  ('Mary', 'Test', 'test8@gamil.com', 3, 0, 'uah'),
+  ('David', 'Jones', 'test9@gamil.com', 2, 0, 'uah'),
+  ('Joe', 'Test', 'test10@gamil.com', 1, 0, 'uah'),
+  ('Olivia', 'Test', 'test11@gamil.com', 2, 0,'uah'),
+  ('Mary', 'Test', 'test12@gamil.com', 2, 0, 'uah'),
+  ('Tom', 'Test', 'test13@gamil.com', 1, 0, 'uah'),
+  ('Lily', 'Test', 'test14@gamil.com', 1, 0, 'uah'),
   (
     'Megan',
     'Brown',
     'test15@gamil.com',
-    1
+    1, 0, 'uah'
   )
+
+  SELECT * FROM "users_with_roles"
   -- SELECT COUNT("user_id"),
   --   "role"
   -- FROM "users_with_roles"
